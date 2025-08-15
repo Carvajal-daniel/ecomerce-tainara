95428ebf042036deb41f7c72d5b6855a94ba7fc1
Carvajal-Daniel <carvajal.dev@hotmail.com>
2025-08-15 19:13:22 -0300

    updated url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,5 @@ import "dotenv/config"
 
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "https://ecomercetainara.vercel.app/api/auth",
+    baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3001/api/auth",
 })
