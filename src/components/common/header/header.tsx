16c1ae2720ebfe8a944287608a2226012031a123
Carvajal-Daniel <carvajal.dev@hotmail.com>
2025-08-25 19:20:51 -0300

    chore: add url categori page

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -3,9 +3,7 @@ import { db } from "@/db";
 import NavPage from "./nav";
 
 const Header = async () => {
-
   const categories = await db.query.categoryTable.findMany();
-
   const mappedCategories = categories.map(category => ({
     id: category.id,
     name: category.name,
@@ -13,12 +11,7 @@ const Header = async () => {
     image: category.imageUrl,
   }));
 
-  return (
-    <header className="bg-white sticky top-0 z-50 border-b border-gray-200 shadow-sm">
-      {/* Navigation Section */}
-      <NavPage categories={mappedCategories}/>
-    </header>
-  );
+  return <NavPage categories={mappedCategories} />;
 };
 
 export default Header;
